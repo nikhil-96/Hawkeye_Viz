3dba88c9d32eef61163b77ddabef9f388743d514
Nikhil <patninikhil047@gmail.com>
2022-01-20 14:06:57 +0100

    merged slider and dropdown code and reduced redundancy

--- apps/exploration.py
+++ apps/exploration.py
@@ -14,15 +14,52 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css'] #css style
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
 
+mapbox_access_token = "pk.eyJ1IjoicGxvdGx5bWFwYm94IiwiYSI6ImNrOWJqb2F4djBnMjEzbG50amg0dnJieG4ifQ.Zme1-Uzoi75IaFbieBDl3A"
+mapbox_style = "mapbox://styles/plotlymapbox/cjvprkf3t1kns1cqjxuxmwixz"
 
+dfa = pd.read_csv('data/dft-road-casualty-statistics-accident-last-5-years.csv')
+# dfa.columns = dfa.columns.str.title()
+uk_cities = json.load(open("data/uk_districts.geojson", "r"))
+dfa = dfa.dropna()
+
+road_guide = pd.read_excel('data/Road-Safety-Open-Dataset-Data-Guide.xlsx')
+district_names = road_guide[road_guide['field name'] == 'local_authority_district']
+district_names.rename(columns={"code/format":"local_authority_district"}, inplace=True)
+dfa['local_authority_district'] = dfa['local_authority_district'].map(district_names.set_index('local_authority_district')['label'])
+
+df1 = dfa
+df1['number_of_accidents'] = 1
+district_names = road_guide[road_guide['field name'] == 'local_authority_ons_district']
+district_names.rename(columns={"code/format": "local_authority_ons_district"}, inplace=True)
+ons_names = district_names[['local_authority_ons_district', 'label']]
+df_final = pd.merge(df1, ons_names, on="local_authority_ons_district", how="left")
+years = dfa['accident_year'].unique()
+cities = df_final['label'].dropna().unique()
+
+print("Exploration")
 layout = html.Div([
+    html.Div([
+        html.Label("Choose a City"),
+        dcc.Dropdown(
+            id='city',
+            options=[
+                {"label": i , "value" : i } for i in cities],
+            placeholder= 'Select a City'),
+        ]),
     html.Div([
         dcc.Graph(id='choropleth-map', style={'width': '60%', 'float': 'left'}, figure={}, clickData=None),
         dcc.Graph(id='accident-graph', style={'width': '40%', 'float': 'right'}, figure={}, clickData=None),
         ]),
     html.Div([
-        # dcc.Graph(id='choropleth-map1', style={'width': '60%', 'float': 'left'}, figure=fig1),
-    ])
+        html.Label("choose a year range"),
+        dcc.RangeSlider(
+            id="year",
+            min=2016,
+            max=2020,
+            value=[2016, 2020],
+            marks={str(i): str(i) for i in years},
+        )
+    ], style={"width": "60%", 'float': 'left'})
 ])
 
     
