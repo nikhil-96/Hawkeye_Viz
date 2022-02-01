ccd290e6780eb9b49c26792f11a8f4bf87d9db11
Nikhil Patni <patninikhil047@gmail.com>
2022-02-01 18:06:29 +0100

    Update exploration.py

--- apps/exploration.py
+++ apps/exploration.py
@@ -18,14 +18,22 @@ mapbox_access_token = "pk.eyJ1IjoicGxvdGx5bWFwYm94IiwiYSI6ImNrOWJqb2F4djBnMjEzbG
 mapbox_style = "mapbox://styles/plotlymapbox/cjvprkf3t1kns1cqjxuxmwixz"
 
 dfa = pd.read_csv('data/dft-road-casualty-statistics-accident-last-5-years.csv')
+dfc = pd.read_csv('data/dft-road-casualty-statistics-casualty-last-5-years.csv')
+dfv = pd.read_csv('data/dft-road-casualty-statistics-vehicle-last-5-years.csv')
+
 # dfa.columns = dfa.columns.str.title()
 uk_cities = json.load(open("data/uk_districts.geojson", "r"))
-dfa = dfa.dropna()
+dfa.dropna(inplace=True)
+dfc.dropna(inplace=True)
+dfv.dropna(inplace=True)
 
 road_guide = pd.read_excel('data/Road-Safety-Open-Dataset-Data-Guide.xlsx')
 district_names = road_guide[road_guide['field name'] == 'local_authority_district']
 district_names.rename(columns={"code/format":"local_authority_district"}, inplace=True)
 dfa['local_authority_district'] = dfa['local_authority_district'].map(district_names.set_index('local_authority_district')['label'])
+road_names = road_guide[road_guide['field name'] == 'road_type']
+road_names.rename(columns={"code/format":"road_type"}, inplace=True)
+dfa['road_type'] = dfa['road_type'].map(road_names.set_index('road_type')['label'])
 
 df1 = dfa
 df1['number_of_accidents'] = 1
@@ -35,6 +43,15 @@ ons_names = district_names[['local_authority_ons_district', 'label']]
 df_final = pd.merge(df1, ons_names, on="local_authority_ons_district", how="left")
 years = dfa['accident_year'].unique()
 cities = df_final['label'].dropna().unique()
+
+#merge datasets of accidents-casualties-vehicles
+casualty_vehicle_df = dfc.merge(dfv, on=['accident_index','vehicle_reference','accident_year','accident_reference'],how='inner')
+
+acc_cas_veh_df = casualty_vehicle_df.merge(dfa, on=['accident_index','accident_year','accident_reference'],how='inner')
+
+cas_sev_names = road_guide[road_guide['field name'] == 'casualty_severity']
+cas_sev_names.rename(columns={"code/format":"casualty_severity"}, inplace=True)
+acc_cas_veh_df['casualty_severity'] = acc_cas_veh_df['casualty_severity'].map(cas_sev_names.set_index('casualty_severity')['label'])
 
 print("Exploration")
 layout = html.Div([
@@ -49,7 +66,13 @@ layout = html.Div([
     html.Div([
         dcc.Graph(id='choropleth-map', style={'width': '60%', 'float': 'left'}, figure={}, clickData=None),
         dcc.Graph(id='accident-graph', style={'width': '40%', 'float': 'right'}, figure={}, clickData=None),
-        ]),
+        dcc.Graph(id='days-hours-graph', style={'width': '50%', 'float': 'left'}, figure={}, clickData=None),
+        dcc.Graph(id='acc-cas-graph',style={'width': '50%', 'float': 'right'}, figure={}, clickData=None),
+        dcc.Graph(id='treemap-graph', style={'width': '50%', 'float': 'left'}, figure={}, clickData=None),
+        dcc.Graph(id='road-speed-graph', style={'width': '50%', 'float': 'right'}, figure={}, clickData=None),
+        dcc.Graph(id='severity-graph', style={'width': '50%', 'float': 'right'}, figure={}, clickData=None)
+
+    ]),
     html.Div([
         html.Label("Choose a Year range"),
         dcc.RangeSlider(
@@ -61,5 +84,3 @@ layout = html.Div([
         )
     ], style={"width": "100%", 'float': 'left', 'color': 'white', 'padding-left':'20%', 'padding-right':'20%'})
 ])
-
-    
