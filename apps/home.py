06a01a9f9802d45cf3d88260a30a7c817edb7f18
Nikhil Patni <patninikhil047@gmail.com>
2022-02-03 18:40:32 +0100

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -1,5 +1,7 @@
 from dash import html
 import dash_bootstrap_components as dbc
+import pandas as pd
+import json
 
 # needed only if running this as a single page app
 #external_stylesheets = [dbc.themes.LUX]
@@ -7,6 +9,12 @@ import dash_bootstrap_components as dbc
 #app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # change to app.layout if running as single page app instead
+dfa = pd.read_csv('data/dft-road-casualty-statistics-accident-last-5-years.csv')
+dfc = pd.read_csv('data/dft-road-casualty-statistics-casualty-last-5-years.csv')
+dfv = pd.read_csv('data/dft-road-casualty-statistics-vehicle-last-5-years.csv')
+road_guide = pd.read_excel('data/Road-Safety-Open-Dataset-Data-Guide.xlsx')
+uk_cities = json.load(open("data/uk_districts.geojson", "r"))
+
 layout = html.Div([
     dbc.Container([
         dbc.Row([
@@ -20,7 +28,10 @@ layout = html.Div([
             ]),
 
         dbc.Row([
-            dbc.Col(html.H5(children='The tool consists of two main pages: Home tab, this is an introduction page to the Group 45 visualization tool. Exploration tab, which gives the oppurtunity to explore the dataset and find interesting patterns')
+            dbc.Col(html.H5(children='The tool consists of two main pages: Home tab, '
+                                     'this is an introduction page to the Group 45 visualization tool. '
+                                     'Exploration tab, which gives the oppurtunity to explore the dataset and '
+                                     'find interesting patterns')
                     , className="mb-5")
         ]),
 
