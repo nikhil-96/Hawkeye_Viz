c643ae35eef352161770940b458a8e6841b146f0
Nikhil Patni <patninikhil047@gmail.com>
2022-02-03 18:40:55 +0100

    Update exploration.py

--- apps/exploration.py
+++ apps/exploration.py
@@ -5,29 +5,20 @@ from dash.dependencies import Input, Output, ClientsideFunction
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.tools import mpl_to_plotly
-import json
 import numpy as np
 import pandas as pd
-
+from apps.home import dfa, dfc, dfv, road_guide
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css'] #css style template
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
 
-mapbox_access_token = "pk.eyJ1IjoicGxvdGx5bWFwYm94IiwiYSI6ImNrOWJqb2F4djBnMjEzbG50amg0dnJieG4ifQ.Zme1-Uzoi75IaFbieBDl3A"
-mapbox_style = "mapbox://styles/plotlymapbox/cjvprkf3t1kns1cqjxuxmwixz"
-
-dfa = pd.read_csv('data/dft-road-casualty-statistics-accident-last-5-years.csv')
-dfc = pd.read_csv('data/dft-road-casualty-statistics-casualty-last-5-years.csv')
-dfv = pd.read_csv('data/dft-road-casualty-statistics-vehicle-last-5-years.csv')
-
 # dfa.columns = dfa.columns.str.title()
-uk_cities = json.load(open("data/uk_districts.geojson", "r"))
+
 dfa.dropna(inplace=True)
 dfc.dropna(inplace=True)
 dfv.dropna(inplace=True)
 
-road_guide = pd.read_excel('data/Road-Safety-Open-Dataset-Data-Guide.xlsx')
 district_names = road_guide[road_guide['field name'] == 'local_authority_district']
 district_names.rename(columns={"code/format":"local_authority_district"}, inplace=True)
 dfa['local_authority_district'] = dfa['local_authority_district'].map(district_names.set_index('local_authority_district')['label'])
@@ -52,6 +43,35 @@ acc_cas_veh_df = casualty_vehicle_df.merge(dfa, on=['accident_index','accident_y
 cas_sev_names = road_guide[road_guide['field name'] == 'casualty_severity']
 cas_sev_names.rename(columns={"code/format":"casualty_severity"}, inplace=True)
 acc_cas_veh_df['casualty_severity'] = acc_cas_veh_df['casualty_severity'].map(cas_sev_names.set_index('casualty_severity')['label'])
+district_names = road_guide[road_guide['field name'] == 'casualty_class']
+district_names.rename(columns={"code/format": "casualty_class"}, inplace=True)
+acc_cas_veh_df['casualty_class'] = acc_cas_veh_df['casualty_class'].map(district_names.set_index('casualty_class')['label'])
+district_names = road_guide[road_guide['field name'] == 'sex_of_casualty']
+district_names.rename(columns={"code/format": "sex_of_casualty"}, inplace=True)
+acc_cas_veh_df['sex_of_casualty'] = acc_cas_veh_df['sex_of_casualty'].map(district_names.set_index('sex_of_casualty')['label'])
+cas_sev = acc_cas_veh_df.groupby(['casualty_severity'])['accident_index'].count().rename('Total Number of Accidents').to_frame()
+
+district_names = road_guide[road_guide['field name'] == 'urban_or_rural_area']
+district_names.rename(columns={"code/format": "urban_or_rural_area"}, inplace=True)
+acc_cas_veh_df.drop(acc_cas_veh_df.loc[acc_cas_veh_df['urban_or_rural_area'] == 3].index, inplace=True)
+acc_cas_veh_df.drop(acc_cas_veh_df.loc[acc_cas_veh_df['urban_or_rural_area'] == -1].index, inplace=True)
+acc_cas_veh_df['urban_or_rural_area'] = acc_cas_veh_df['urban_or_rural_area'].map(
+    district_names.set_index('urban_or_rural_area')['label'])
+district_names = road_guide[road_guide['field name'] == 'road_surface_conditions']
+district_names.rename(columns={"code/format": "road_surface_conditions"}, inplace=True)
+acc_cas_veh_df.drop(acc_cas_veh_df.loc[acc_cas_veh_df['road_surface_conditions'] == -1].index, inplace=True)
+acc_cas_veh_df['road_surface_conditions'] = acc_cas_veh_df['road_surface_conditions'].map(
+    district_names.set_index('road_surface_conditions')['label'])
+district_names = road_guide[road_guide['field name'] == 'weather_conditions']
+district_names.rename(columns={"code/format": "weather_conditions"}, inplace=True)
+acc_cas_veh_df.drop(acc_cas_veh_df.loc[acc_cas_veh_df['weather_conditions'] == -1].index, inplace=True)
+acc_cas_veh_df['weather_conditions'] = acc_cas_veh_df['weather_conditions'].map(
+    district_names.set_index('weather_conditions')['label'])
+district_names = road_guide[road_guide['field name'] == 'light_conditions']
+district_names.rename(columns={"code/format": "light_conditions"}, inplace=True)
+acc_cas_veh_df.drop(acc_cas_veh_df.loc[acc_cas_veh_df['light_conditions'] == -1].index, inplace=True)
+acc_cas_veh_df['light_conditions'] = acc_cas_veh_df['light_conditions'].map(
+    district_names.set_index('light_conditions')['label'])
 
 print("Exploration")
 layout = html.Div([
@@ -77,12 +97,12 @@ layout = html.Div([
     ], style=dict(display='flex')),
     html.Div([
         dcc.Graph(id='choropleth-map', style={'width': '60%', 'float': 'left'}, figure={}, clickData=None),
-        dcc.Graph(id='accident-graph', style={'width': '40%', 'float': 'right'}, figure={}, clickData=None),
+        dcc.Graph(id='severity-graph', style={'width': '40%', 'float': 'right'}, figure={}, clickData=None),
         dcc.Graph(id='days-hours-graph', style={'width': '50%', 'float': 'left'}, figure={}, clickData=None),
-        dcc.Graph(id='acc-cas-graph',style={'width': '50%', 'float': 'right'}, figure={}, clickData=None),
+        dcc.Graph(id='acc-cas-graph', style={'width': '50%', 'float': 'right'}, figure={}, clickData=None),
         dcc.Graph(id='treemap-graph', style={'width': '50%', 'float': 'left'}, figure={}, clickData=None),
         dcc.Graph(id='road-speed-graph', style={'width': '50%', 'float': 'right'}, figure={}, clickData=None),
-        dcc.Graph(id='severity-graph', style={'width': '50%', 'float': 'left'}, figure={}, clickData=None),
-        dcc.Graph(id='violin-plot', style={'width': '50%', 'float': 'right'}, figure={}, clickData=None)
+        dcc.Graph(id='violin-graph', style={'width': '50%', 'float': 'left'}, figure={}, clickData=None),
+        dcc.Graph(id='factors-graph', style={'width': '50%', 'float': 'right'}, figure={}, clickData=None)
     ])
 ])
